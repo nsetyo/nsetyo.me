04c3026a0078e052377e00cec98e2d79ab21ebf8
Setyo <setyo@pm.me>
2022-05-19 13:57:12 +0700

    Upgrade tailwindcss

--- .bashrc
+++ .bashrc
@@ -1,1 +1,5 @@
 export PATH=/var/www/themes/sty/node_modules/.bin:$PATH
+
+if [ -f "/etc/bash_completion" ] ; then
+    . "/etc/bash_completion"
+fi
--- themes/sty/tailwind.config.js
+++ themes/sty/tailwind.config.js
@@ -1,31 +1,3 @@
-const { colors } = require('tailwindcss/defaultTheme');
-
 module.exports = {
-    purge: {
-        enabled: true,
-        content: [
-            __dirname + '/layouts/**/*.html'
-        ],
-    },
-    theme: {
-        colors: {
-            black: colors.black,
-            gray: colors.gray,
-            indigo: colors.indigo,
-            transparent: colors.transparent,
-            white: colors.white,
-        },
-        container: {
-            center: true,
-            padding: '1.5rem'
-        },
-        fontFamily: {
-            sans: "'Source Sans Pro', sans-serif",
-            mono: "'Source Code Pro', monospace"
-        },
-        screens: {
-            'sm': '640px',
-            // => @media (min-width: 640px) { ... }
-        }
-    },
+    content: [ '../../content/**/*.md', 'layouts/**/*.html' ],
 }
