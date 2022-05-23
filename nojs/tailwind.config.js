083b77f7422034818a53280285ed7c0d3dfcff93
Setyo <setyo@pm.me>
2022-05-23 14:07:50 +0700

    Fix container

--- nojs/tailwind.config.js
+++ nojs/tailwind.config.js
@@ -3,26 +3,28 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
     content: ['content/**/*.md', 'themes/**/*.{html,svg}'],
     theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+        },
         extend: {
             typography: (theme) => ({
                 DEFAULT: {
                     css: {
                         a: {
                             '&:hover': {
-                                'color': theme('colors.indigo.500'),
+                                color: theme('colors.indigo.500'),
                             },
                         },
-                    // ...
-                  },
+                        // ...
+                    },
                 },
-              }),
+            }),
         },
         fontFamily: (theme) => ({
             mono: ['Fira Code', ...defaultTheme.fontFamily.mono],
             sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
         }),
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
+    plugins: [require('@tailwindcss/typography')],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,28 @@ module.exports = {
     content: ['content/**/*.md', 'themes/**/*.{html,svg}'],
     darkMode: 'class',
     theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+        },
         extend: {
             typography: (theme) => ({
                 DEFAULT: {
                     css: {
                         a: {
                             '&:hover': {
-                                'color': theme('colors.indigo.500'),
+                                color: theme('colors.indigo.500'),
                             },
                         },
-                    // ...
-                  },
+                        // ...
+                    },
                 },
-              }),
+            }),
         },
         fontFamily: (theme) => ({
             mono: ['Fira Code', ...defaultTheme.fontFamily.mono],
             sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
         }),
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
+    plugins: [require('@tailwindcss/typography')],
 }
